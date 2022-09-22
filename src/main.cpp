4952de83dd520a73025c2661d67638f1ad7490c0
Brett Boyd <79927828+bboyd01@users.noreply.github.com>
2022-09-22 17:53:32 +0000

    removed comment from line 1

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,34 @@
-// hey guys this is v2. I hope this works
-
+#include <iostream>
 #include <SFML/Graphics.hpp>
 
-int main()
-{  
-    sf::RenderWindow window(sf::VideoMode(1920, 1080), "Dog Dash!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color (0xFF0000FF));
+int main() {
+  sf::RenderWindow window(sf::VideoMode(1920, 1080), "Dog Dash!");
+  sf::CircleShape shape(100.f);
 
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
+  // //Justin's code start
+  // sf::Sprite dog;  
+  // sf::Texture texture;
+  // sf::Image image;
+  // image.loadFromFile("https://replit.com/@sfml-epic-gamers/"
+  //                    "SFML-Epic-Game-v2#Dog%20WIP%201%20500.png");
+  // texture.loadFromImage(image, const IntRect& area = IntRect());
+  // dog.setTexture(texture);
+  // shape.setFillColor(sf::Color(0xFF0000FF));  
+  // //Justin's code end
 
-        window.clear();
-        window.draw(shape);
-        window.display();
+  
+  while (window.isOpen()) {
+    sf::Event event;
+    while (window.pollEvent(event)) {
+      if (event.type == sf::Event::Closed)
+        window.close();
     }
 
-    return 0;
+    window.clear();
+    window.draw(shape);
+    //window.draw(dog);  //Drawing Justin's dog
+    window.display();
+  }
+
+  return 0;
 }
